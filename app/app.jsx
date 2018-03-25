51516fd4d2753861659401639f1100c51642fbcf
Glitch (starter-react-redux) <none>
2018-03-25 01:03:31 -0700

    babel and canvas

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,22 @@ const Route = require('react-router-dom').Route;
 const BrowserRouter = require('react-router-dom').BrowserRouter;
 const hashHistory = require('react-router-dom').hashHistory;
 
+//deep-dive
+const books = [
+  { id: 1, title: 'Book 1', price: 1000 },
+  { id: 2, title: 'Book 2', price: 2000 },
+  { id: 3, title: 'Book 3', price: 3000 },
+  { id: 4, title: 'Book 4', price: 4000 }
+];
+
+const BookList = ({books}) => {
+  return (
+    React.createElement("div", {className: "book-list"}, "Books go here")
+  );
+}
+
+//deep-dive
+
 // redux
 const { createStore } = require('redux');
 const { Provider } = require('react-redux');
@@ -26,3 +42,7 @@ render((
       </div>
     </BrowserRouter>
   </Provider>), document.getElementById('main'));
+
+render(React.createElement(BookList),
+  document.getElementById('react')
+);
